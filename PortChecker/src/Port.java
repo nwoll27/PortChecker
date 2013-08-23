c784677158f2f321c21bc7fbe3afcdbcc9461457
nwoll <nwoll27@gmail.com>
2013-08-23 17:15:04 -0400

    Added new Port and PortChecker classes, changed ports.csv to use one cell per entry.

---
+++ PortChecker/src/Port.java
@@ -0,0 +1,23 @@
+
+public class Port {
+	private int port;
+	private boolean isOpen;
+	
+	public Port(int port){
+		this.port = port;
+		isOpen = true;
+	}
+	
+	public Port(int port, boolean isOpen){
+		this.port = port;
+		this.isOpen = isOpen;
+	}
+	
+	public String toString(){
+		String status = "CLOSED";
+		if(isOpen){
+			status = "OPEN";
+		}
+		return "Port " + port + " is " + status;
+	}
+}
---
+++ PortChecker/src/PortChecker.java
@@ -0,0 +1,18 @@
+
+public class PortChecker {
+	
+	public void checkPorts(){
+		processInput();
+		
+		buildReport();
+	}
+	
+	public void processInput(){
+		
+	}
+	
+	public void buildReport(){
+		
+	}
+	
+}
